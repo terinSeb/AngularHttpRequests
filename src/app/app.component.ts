df7bdc83554ddc2f673c72b8507b0d8f5fffe3ea
Terin Sebastin <terin01@gmail.com>
2022-03-06 12:20:25 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularHttpRequest';
+  loadedPosts = [];
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {}
+  onCreatePost(postData: { title: string; content: string }) {}
+  onFetchPosts() {}
+  onClearPosts() {}
 }
